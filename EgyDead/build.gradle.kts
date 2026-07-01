146def7fde60e07681c4912c88a917ef3452b00e
Zero1sui <ez613129@gmail.com>
2026-07-02 00:07:57 +0300

    Refactor build.gradle.kts by removing comments and unused lines

--- EgyDead/build.gradle.kts
+++ EgyDead/build.gradle.kts
@@ -1,27 +1,12 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // THIS LINE IS THE HOLY GRAIL FOR REPO DEPLOYMENT
-    className = "EgyDeadPlugin" 
-    
     description = "Watch OUT DAWG!"
     authors = listOf("ZERO")
-
-    /**
-     * Status int as one of the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta-only
-     **/
     status = 1 
-
     tvTypes = listOf("Movie", "TvSeries", "Anime")
-    // you can swap this icon url out later for an egydead logo if you want
     iconUrl = "https://tv9.egydead.live/favicon.ico"
-
     isCrossPlatform = true
 }
